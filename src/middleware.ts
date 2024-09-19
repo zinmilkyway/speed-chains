ab71b2c7161b74cee71387d63cc92a58ac5b27f8
thanhhd@aladintech.co <thanhhd@aladintech.co>
2024-09-19 23:47:34 +0700

    fix build

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,13 +29,13 @@ export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  if (
-    [
-      "/logo.png",
-      // Your other files in `public`
-    ].includes(pathname)
-  )
-    return;
+  // if (
+  //   [
+  //     "/logo.png",
+  //     // Your other files in `public`
+  //   ].includes(pathname)
+  // )
+  //   return;
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
@@ -60,5 +60,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|logo.png).*)"],
+  // matcher: ["/((?!api|_next/static|_next/image|logo.png).*)"],
 };
